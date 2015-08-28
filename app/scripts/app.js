b73286747345a49e6f0d039b53db87143796d5ef
Erperejildo <info@danielrodriguez.eu>
2015-08-28 12:17:57 +0200

    Scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name itunesBooksApp
+ * @description
+ * # itunesBooksApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('itunesBooksApp', [
+    'ngAnimate',
+    'ngSanitize'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name itunesBooksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the itunesBooksApp
+ */
+angular.module('itunesBooksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
